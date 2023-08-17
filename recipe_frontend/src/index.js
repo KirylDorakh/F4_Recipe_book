c89322bc58cc68a32bf5333904f064a5fc3c2d90
Kiryl Dorakh <kirill.dorokh@gmail.com>
2023-08-17 09:00:27 +0400

    frontend init

---
+++ recipe_frontend/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {createRoot} from "react-dom/client";
+
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import App from "./routes/App";
+
+const root = createRoot(document.getElementById("root"))
+
+root.render(
+    <App />
+)
